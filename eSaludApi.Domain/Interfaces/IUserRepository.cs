a6841892e64e3abc94080f0f70d9286aa04adacb
monroydiego91 <diego.monroyg@uanl.edu.mx>
2026-01-02 22:22:39 -0600

    feat: se agregó la Interfaz de User a la capa Domain

--- eSaludApi.Domain/Interfaces/IUserRepository.cs
+++ eSaludApi.Domain/Interfaces/IUserRepository.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using eSaludApi.Domain.Common.Entities;
 
-namespace eSaludApi.Domain.Interfaces
+namespace eSaludApi.Domain.Interfaces;
+
+public interface IUserRepository
 {
-    internal class IUserRepository
-    {
-    }
+    Task<User?> GetByEmailAsync(string email);
+    Task AddAsync(User user);
+    Task SaveChangesAsync();
+
 }
